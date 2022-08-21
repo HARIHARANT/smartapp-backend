7bc9375d8b7256ccdf2aca59d63f2749eb8fb50c
Hariharan Thangavel <30747099+HARIHARANT@users.noreply.github.com>
2022-08-21 16:07:23 +0530

    Delete router directory

--- router/userRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userModule = require("../modules/userMod")
-router.get("/get",userModule.getUsers);
-router.post("/set",userModule.setUser);
-module.exports = router;
