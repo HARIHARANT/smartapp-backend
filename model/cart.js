8d5a0fb17eda66c9bd83f1688aea88afdc91daa4
Hariharan Thangavel <30747099+HARIHARANT@users.noreply.github.com>
2022-08-23 07:47:23 +0530

    Update cart.js

--- model/cart.js
+++ model/cart.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({   
     userId:{type:String, default:null},
-    productId:{type:String, default:null},
+    productId:{type:Number, default:null},
     createdAt:{type:Date, default:Date.now}
 });
 
